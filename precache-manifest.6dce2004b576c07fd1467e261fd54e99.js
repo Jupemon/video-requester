7957d5709253369767b13dce87f74dcb9fa230c2
Juho <s6heju04@gmail.com>
2020-02-28 11:59:02 +0200

    Updates

---
+++ precache-manifest.6dce2004b576c07fd1467e261fd54e99.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "435cf00d19a3f820692f6de0a9b659a4",
+    "url": "/my-app/requester/index.html"
+  },
+  {
+    "revision": "2022d04741d20f5ef11a",
+    "url": "/my-app/requester/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "dbf30cd5620acf529227",
+    "url": "/my-app/requester/static/css/main.4d850cfd.chunk.css"
+  },
+  {
+    "revision": "2022d04741d20f5ef11a",
+    "url": "/my-app/requester/static/js/2.b21a58be.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/my-app/requester/static/js/2.b21a58be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbf30cd5620acf529227",
+    "url": "/my-app/requester/static/js/main.12ca25e7.chunk.js"
+  },
+  {
+    "revision": "7709cacf050971626ebf",
+    "url": "/my-app/requester/static/js/runtime-main.395acacc.js"
+  }
+]);
